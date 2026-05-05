87ff8b4fdf4285bb126dce2a6084f70da5646fca
iRay1h <andresf.echeverric08gmail.com>
2026-05-05 11:07:09 -0500

    feat: creacion inicial del proyecto con spring boot

---
+++ src/main/java/com/andres/supermarket_jwt/SupermercadoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.andres.supermarket_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/supermarket_jwt/SupermercadoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.supermarket_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
