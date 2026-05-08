189abc1b421e356cf529a4af3ac14e33dfd560e5
dayronF <dayronflorez0@gmail.com>
2026-05-07 21:04:08 -0500

    feat: Creacion de etidades de la tabla productos

---
+++ src/main/java/com/andres/supermarket_jwt/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.andres.supermarket_jwt.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "barcode")
+    private String barcode;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "stock")
+    private Integer stock;
+    @Column(name = "state")
+    private Boolean state;
+   
+}
